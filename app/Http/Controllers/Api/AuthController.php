0df1c268f84b6d81d15c16f52ba5c706973e5e57
Seemo0 <medouallal08@gmail.com>
2022-12-12 19:51:54 +0100

    config for backen

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request) {
-
+    public function login(LoginRequest $request) 
+    {
+        
     }
 
     public function signup(SignupRequest $request) {

74c217ac382058cda21a35dd96242dd0a45b1d7e
Seemo0 <medouallal08@gmail.com>
2022-12-12 19:09:05 +0100

    implement user CRUD on backend

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,40 @@
+import { Link, Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../context/ContextProvider'
+
+export default function DefaultLayout() {
+    const {user, token} = useStateContext()
+
+    if(!token) {
+        return <Navigate to="/login" />
+    }
+
+    const onLogout = (e) => {
+      e.preventDefault()
+       
+    }
+
+  return (
+    <div id='defaultLayout'>
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>
+            Header
+          </div>
+
+          <div>
+            {user.name}
+            <a onClick={onLogout} className="btn-logout" href="#">Logout</a>
+          </div>
+        </header>
+        <main>
+          
+          <Outlet/>
+        </main>
+      </div>
+    </div>
+  )
+}

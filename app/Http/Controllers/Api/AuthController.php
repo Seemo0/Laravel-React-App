56efc8ed048582bd10e2a18c20630a476aa21bc9
Seemo0 <medouallal08@gmail.com>
2022-12-12 23:11:24 +0100

    congig for backend

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
+use http\Env\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -25,11 +27,24 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request) 
     {
+        $credentials = $request->validated();
+        if(!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided email adress or password is incorrect',
+            ], 422);
+        };
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
         
     }
-    
+
 
     public function logout(Request $request) {
-        
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response('', 204);
     }
-}
+} 

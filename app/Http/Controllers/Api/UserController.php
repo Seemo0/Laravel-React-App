1d1074d87d5966ed03c7477e67d7011180880121
Seemo0 <medouallal08@gmail.com>
2022-12-14 17:47:04 +0100

    changes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 
 class UserController extends Controller
@@ -12,11 +13,11 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        return UserResource::collection(User::query()->orderBy('id', 'desc')->paginate(10));
     }
 
     /**
@@ -27,7 +28,11 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+
+        return response(new UserResource($user), 201);
     }
 
     /**
@@ -38,7 +43,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
+        return new UserResource($user);
     }
 
     /**
@@ -50,7 +55,14 @@ class UserController extends Controller
      */
     public function update(UpdateUserRequest $request, User $user)
     {
-        //
+        $data = $request->validated();
+        if(isset($data['password'])) {
+        $data['password'] = bcrypt($data['password']); 
+        }
+        $user->update($data);
+
+        return new UserResource($user);
+
     }
 
     /**
@@ -61,6 +73,8 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return response("", 204);
     }
 }

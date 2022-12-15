d287c891aae348d03aa7373bcc9b0b499bb7c909
Seemo0 <medouallal08@gmail.com>
2022-12-15 19:33:00 +0100

    implementing user crud on front end

--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export default function UserForm() {
+  const {id} = useParams()
+  const navigate = useNavigate()
+  const [user, setUser] = useState({
+    id: null,
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+  })
+  const [errors, setErrors] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const {setNotification} = useStateContext()
+
+  if(id) {
+    
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
   return (
-    <div>UserForm</div>
+    <>
+    <div className='card animated fadeInDown'>
+      {loading && (
+        <div className='text-center'>
+          Loading...
+        </div>
+      )}
+      {errors &&
+          <div className="alert">
+            {Object.keys(errors).map(key => (
+              <p key={key}>{errors[key][0]}</p>
+            ))}
+          </div>
+        }
+        {!loading && (
+          <form onSubmit={onSubmit}>
+            <input value={user.name} onChange={ev => setUser({...user, name: ev.target.value})} placeholder="Name"/>
+            <input value={user.email} onChange={ev => setUser({...user, email: ev.target.value})} placeholder="Email"/>
+            <input type="password" onChange={ev => setUser({...user, password: ev.target.value})} placeholder="Password"/>
+            <input type="password" onChange={ev => setUser({...user, password_confirmation: ev.target.value})} placeholder="Password Confirmation"/>
+            <button className="btn">Save</button>
+          </form>
+        )}
+    </div>
+    </>
   )
 }
 
